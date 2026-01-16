1d2e567fc019dd6cb57c72515a97642612716402
Sunshine <sunshine.17923@gmail.com>
2026-01-16 19:13:11 +0700

    1

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,5 +1,47 @@
 import axios from 'axios'
 
-export function getPostalAddress() {
-    const res = await 
+interface Data {
+    id: number
+    name: string
+    phone: number
+    address: addressData 
 }
+
+interface addressData {
+    street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: number
+            lng: number
+}}
+
+interface returnData {
+    id: number
+    name: string
+    phone: number
+    address: addressData 
+}
+
+export async function getPostalAddress(): Promise <returnData[]> {
+    try {
+    const res = await axios.get<Data[]>("https://jsonplaceholder.typicode.com/users")
+    const getData = res.data
+    if(!getData || getData.length === 0) {
+        return []
+    }
+    
+    return getData.map(s => ({
+        id: s.id,
+        name: s.name,
+        phone: s.phone,
+        address: s.address
+    }))
+
+    } catch (error) {
+        return []
+    }
+}
+
+console.log(getPostalAddress())
